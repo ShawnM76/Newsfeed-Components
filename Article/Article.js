1cc1894ef199449fa7b1e6b71a8c6292476e5695
Shawn McManus <xsvfan76@gmail.com>
2019-07-10 16:40:02 -0600

    finished making JSOP and some javascript

--- Article/Article.js
+++ Article/Article.js
@@ -1,25 +1,25 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton');
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => {
-    this.expandArticle();
-    })
-  }
+// class Article {
+//   constructor(domElement) {
+//     // assign this.domElement to the passed in domElement
+//     this.domElement = domElement;
+//     // create a reference to the ".expandButton" class. 
+//     this.expandButton = domElement.querySelector('.expandButton');
+//     // Using your expandButton reference, update the text on your expandButton to say "expand"
+//     this.expandButton.textContent = "expand";
+//     // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     this.expandButton.addEventListener('click', () => {
+//     this.expandArticle();
+//     })
+//   }
 
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
-    //this.domElement.classList.toggle('close');
-  }
-}
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//     this.domElement.classList.toggle('article-open');
+//     //this.domElement.classList.toggle('close');
+//   }
+// }
 
 /* START HERE: 
 
@@ -29,9 +29,39 @@ class Article {
 
 */
 
-let articles = document.querySelectorAll('.article');
+// let articles = document.querySelectorAll('.article');
 
-articles.forEach(e => {
-  return new Article(e);
-});
+// articles.forEach(e => {
+//   return new Article(e);
+// });
 
+const accordian = document.querySelector('.articles')
+
+accordian.appendChild(panelComponent());
+
+function panelComponent() {
+  const articles = document.createElement('div');
+  const article = document.createElement('div');
+  const title = document.createElement('h2');
+  const paragraph1 = document.createElement('p');
+  const paragraph2 = document.createElement('p');
+  const paragraph3 = document.createElement('p');
+  const paragraph4 = document.createElement('p');
+  const button = document.createElement('span');
+
+  articles.appendChild(article);
+  articles.appendChild(paragraph1);
+  articles.appendChild(paragraph2);
+  articles.appendChild(paragraph3);
+  articles.appendChild(paragraph4);
+  article.appendChild(title);
+  article.appendChild(button);
+
+  articles.classList.add('.articles');
+  article.classList.add('.article');
+  button.classList.add('.expandButton');
+
+  
+  return articles;
+
+}
